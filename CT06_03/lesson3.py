1c7da92490f5744c96ea514d6df33c557b713d34
Computhink-CS136 <cs136@computhink.com.sg>
2026-01-10 07:32:08 +0000

    Lesson 1

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,32 @@
-print("Hello from lesson 3")
+# task 1
+# age = 13
+# print(age)
+# x = 10
+# x = 20
+# print(x)
+# task 2
+# variable1 = 3
+# variable2 = 5
+# print(variable1 + variable2)
+# print(2 + 4 * 5 / 6 + 3 - 6 / 2)
+# task 3
+# a = 34
+# b = 35
+# ans = a + b
+# print(ans)
+# c = 10
+# d = 50
+# ee = 3
+# ans = (d - c) * ee
+# print(ans)
+# e = 100
+# f = 4
+# ans = e / f
+# print(ans)
+# g = 3
+# h = 4
+# i = 5
+# j = 10
+# k = 2
+# ans = (g + f) * i - (j / k)
+# print(ans)
