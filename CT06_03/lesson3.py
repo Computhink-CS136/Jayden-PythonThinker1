69b52e84f90b41de58501d19887049a6cfb75353
Computhink-CS136 <cs136@computhink.com.sg>
2026-01-17 07:31:22 +0000

    Lesson 2

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,3 +1,5 @@
+import math
+
 # task 1
 # age = 13
 # print(age)
@@ -9,7 +11,7 @@
 # variable2 = 5
 # print(variable1 + variable2)
 # print(2 + 4 * 5 / 6 + 3 - 6 / 2)
-# task 3
+# task 4
 # a = 34
 # b = 35
 # ans = a + b
@@ -30,3 +32,39 @@
 # k = 2
 # ans = (g + f) * i - (j / k)
 # print(ans)
+# task 3
+# number = round(42.56789,2)
+# print(number)
+# number = round(1267.15)
+# print(number)
+# number = round(1785.956,3)
+# print(number)
+# task 5
+# pine_tart = 15 * 2
+# bak_kwa = 58
+# total = pine_tart + bak_kwa
+# print(total)
+# spent = 96 + (18 * 3) + (6 * 2)
+# before_vouch = spent * 1.1
+# now = before_vouch - 25
+# rlly_now = round(now,2)
+# print(rlly_now)
+# spent = (100 * 5) + (20 * 8) + (20 * 2.50)
+# service = spent * 1.1
+# gst = service * 1.09
+# full = round(gst,2)
+# print(full)
+# print(abs(-55))
+# print(abs(-8))
+# e = math.floor(9.8)
+# batches = e / 1
+# print(batches)
+# bag = 27 / 4
+# minimum = math.ceil(bag)
+# print(minimum)
+# ran = math.pi * 100
+# e = round(ran,2)
+# print(e)
+# area = 12 ** 2 * math.pi
+# twodec = round(area,2)
+# print(twodec)
--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -1,1 +1,9 @@
-print("Hello from lesson 4")
+# red = 1 * 3
+# blue = 2 * 5
+# green = 3 * 4
+# total = red + blue + green
+# print(total)
+# name = "Jayden"
+# print("My name iss + name")
+myName = input("What is your name?")
+print("Hello," +  myName)
