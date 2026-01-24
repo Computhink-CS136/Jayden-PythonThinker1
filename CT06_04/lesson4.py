41a49ae6a40699c6ce5c96ddecb197bcbcda3b33
Computhink-CS136 <cs136@computhink.com.sg>
2026-01-24 07:33:29 +0000

    Lesson 3

--- CT06_04/lesson4.py
+++ CT06_04/lesson4.py
@@ -2,8 +2,57 @@
 # blue = 2 * 5
 # green = 3 * 4
 # total = red + blue + green
-# print(total)
-# name = "Jayden"
-# print("My name iss + name")
-myName = input("What is your name?")
-print("Hello," +  myName)
+# myName = input("What is your name?")
+# print("Hello," +  myName)
+# number = input("what is yo favourite numba")
+# # print(number)
+# # task 1a
+# name = input("WHat is your name?\n") # \n go to new line
+# print(name)
+# # task 1b
+# colour = input("what is yo fav colour?\n")
+# print(colour)
+# # task 1c
+# age = input("What is your age?\n")
+# print(age)
+# task 2a
+# name = input("what is your name\n")
+# print("Hi, " + name + "! Nice to meet you")
+# task 2b
+# hobby = input("what is yo hobby\n")
+# print("so you like " + hobby + ", nice")
+# task 2c
+# dream_vaca = input("what vacation is ur dream\n")
+# print("thats a great place to go to, " + dream_vaca + ", buy me a ticket there with u")
+# task 3a
+# # age = input("what is yo age?\n")
+# age =int(input("what is yo age\n"))
+# age = age + 1
+# age = str(age)
+# print("next year u are " + age + " years old")
+# # task 3b
+# num = int(input("gimme a numba\n"))
+# num = num * 2
+# num = str(num)
+# print("The number u chose x 2 with is " + num + ". pretty cool" )
+# task 3c
+# year = int(input("gimme the year u were born\n"))
+# year = 2026 - year
+# year = str(year)
+# print("Your age should be  " + year + " from my calculations")
+# task 4
+# message = str("Hello")
+# message = int("10")\
+# print(message)
+# task 5
+# firstname = "Jayden"
+# lastname = "Lim"
+# print(firstname + lastname)
+# task 6
+# user_name = "Jayden"
+# user_age = "13"
+# print(user_name   + " is " + user_age)
+width = input("width of rectangle\n")
+length = input("length of rectangle\n")
+area = width * length
+print("your area of the rectangle is" + area)
