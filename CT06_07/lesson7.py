c9389b19d1416e20c9ab15c1ec7b89a5f357c806
Computhink-CS136 <cs136@computhink.com.sg>
2026-02-14 07:39:12 +0000

    Co-authored-by: trainer1-computhink <trainer1-computhink@users.noreply.github.com>

--- CT06_07/lesson7.py
+++ CT06_07/lesson7.py
@@ -22,7 +22,69 @@
 
 # recap 3
 
-firstname = input("Enter your name:\n")
-lastname = input("Enter your last name:\n")
-age = int(input("Enter your age:\n"))
-print(f"Hello, {firstname} {lastname}! You are {age} years old.")
+# firstname = input("Enter your name:\n")
+# lastname = input("Enter your last name:\n")
+# age = int(input("Enter your age:\n"))
+# print(f"Hello, {firstname} {lastname}! You are {age} years old.")
+
+# # task 1
+# for i in range(1, 11):
+#     print(i)
+# # task 2
+# for i in range(2, 21, 2):
+#     print(i)
+# # task 3
+# for i in range(10, 0, -1):
+#     print(i)
+# # task 4
+# numoftime = int(input("Enter the number of times to print\n"))
+# saywhat = input("Enter the word to print\n")
+# for i in range(numoftime):
+#     print(saywhat)
+# # task 5
+# numoftime = int(input("Enter the number of times to print\n"))
+# name = input("Enter the name to print\n")
+# for i in range(numoftime):
+#     print("hello " + name)
+# task 6
+# sum = 0
+# for i in range(1, 6):
+#     num = int(input(f"Enter number {i} \n"))
+#     sum = sum + num
+# print(f"The sum of the numbers is: {sum}")
+#task 7
+# num = int(input("Enter a number:\n"))
+# print(f"Multiplication table for {num}:")
+# for i in range(1, 13):
+#     print(f"{num} x {i} = {num * i}")
+
+#task 8
+# num = int(input("Enter a number:\n"))
+
+# for i in range(num + 1):
+#     print(str(i) * i)
+
+#task 9
+# sumof = 0
+# for i in range(1, 6):
+#     num = int(input(f"Enter number {i} \n"))
+#     sumof = sumof + num
+# average = sumof / 5
+# print(f"The average of the numbers is: {average}")
+
+# task 10
+# sumof = 0
+# noofscores = int(input("Enter the number of scores:\n"))
+# for i in range(1, noofscores + 1):
+#     num = int(input(f"Enter number {i} \n"))
+#     sumof = sumof + num
+# average = sumof / 5
+# print(f"The average of the numbers is: {average}")
+
+# task 11
+# num = int(input("Enter a number:\n"))
+
+# for i in range(num + 1):
+#     print(str(i) * i)
+# for i in range(num - 1, 0, -1):
+#     print(str(i) * i)
