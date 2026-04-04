0fcf0023d651115283787202f41778c77de0ddb6
Computhink-CS136 <cs136@computhink.com.sg>
2026-04-04 07:34:02 +0000

    Lesson 9

--- CT06_10/lesson10.py
+++ CT06_10/lesson10.py
@@ -1,1 +1,59 @@
-print("Hello from lesson 10")
+# #task 1
+# number = int(input("Enter a number: "))
+# if number > 0:
+#     print(f"The number {number} is positive.")
+# elif number < 0:
+#     print(f"The number {number} is not positive.")
+# task 2
+# age = int(input("Enter your age: "))
+# if age < 13:
+#     print("You are a child.")
+# elif age > 13 and age < 20:
+#     print("You are a teenager.")
+# else:
+#     print("You are an adult.")
+# task 3
+# temperature = int(input("Enter the temperature in Celsius: "))
+# if temperature > 30:
+#     print("You can go swimming.")
+# elif temperature > 20 and temperature < 30:
+#     print("You can play basketball.")
+# elif temperature > 20 and temperature < 24:
+#     print("You can go cycling.")
+# elif temperature < 20:
+#     print("You should stay indoors.")
+# task 4
+# SCORE = int(input("Enter your score: "))
+# if SCORE > 90:
+#     print("Your grade is A.")
+# elif SCORE > 80 and SCORE < 90:
+#     print("Your grade is B.")
+# elif SCORE > 70 and SCORE < 80:
+#     print("Your grade is C.")
+# elif SCORE > 60 and SCORE < 70:
+#     print("Your grade is D.")
+# elif SCORE < 60:
+#     print("Your grade is F.")
+# task 5
+# age = int(input("Enter your age: "))
+# if age < 0:
+#     print("Invalid age.")
+#     print("Age cannot be negative.")
+# elif age >= 18:
+#     print("You are an adult.")
+#     print("You can vote.")
+# else:
+#     print("You are a minor.")
+#     print("You cannot vote.")
+# task 6
+# money = int(input("Enter the amount of money you have: "))
+# if money >= 150:
+#     print("You can buy a new gaming keyboard.")
+# elif money >= 100:
+#     print("You can buy a new game.")
+# elif money >= 50:
+#     print("You can buy a new gaming mouse.")
+# elif money >= 20:
+#     print("You can buy a new gaming headset.")
+# else:
+#     print("You can only buy snacks lmao.")
--- CT06_11/lesson11.py
+++ CT06_11/lesson11.py
@@ -1,1 +1,52 @@
-print("Hello from lesson 11")
+# task 1
+# height1 = int(input("Enter the height of the first rider: "))
+# height2 = int(input("Enter the height of the second rider: "))
+# if height1 >= 120 and height2 >= 120:
+#     print("You can ride the roller coaster together!")
+# else:    print("Sorry, you cannot ride the roller coaster together.")
+# task 2
+# num = int(input("Enter a number: "))
+# if num % 3 == 0 and num % 7 == 0:
+#     print("The number is divisible by both 3 and 7.")
+# else:
+#     print("The number is not divisible by both 3 and 7.")
+# task 3
+# firstname = input("Enter your first name: ").lower()
+# lastname = input("Enter your last name: ").lower()
+# if firstname == "james" and lastname == "leong":
+#     print("YOU ARE WANTED!")
+# task 4
+# rider1 = int(input("Enter the age of the first rider: "))
+# rider2 = int(input("Enter the age of the second rider: "))
+# if (rider1 >= 18 or rider2 >= 18):
+#     print("You can ride the go kart together!")
+# else:
+#     print("Sorry, you cannot ride the go kart together.")
+# task 5
+# age = int(input("Enter your age: "))
+# if age < 0:
+#     print("Invalid age. Age cannot be negative.")
+# if age >= 13 and age < 65:
+#     print("You are an adult. Pay $20 for the ticket.")
+# elif age <= 12 or age >= 65:
+#     print("You are a child or senior. Pay $10 for the ticket.")
+# task 6
+# gender = input("Enter your gender: ").lower()
+# if  gender == "m" or gender == "male":
+#     print("valid input")
+# else    print("invalid input")
+# task 9
+# passwordkey = "Python123"
+# password = input("Enter the password: ")
+# if password == passwordkey:
+#     print("Access granted.")
+# else:    print("Access denied. Incorrect password.")
+johnusername = "John123"
+johnpassword = "pw123"
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+if username == johnusername and password == johnpassword:
+    print("Access granted.")
+elif username == johnusername or password != johnpassword:
+    print("Username or password incorrect.")
+else:    print("Access denied. Incorrect username and password.")
